fa24987f41cb7597f047891270cf0ac1f02d2dd8
Omnish22 <omnishkumar@Omnishs-MacBook-Pro.local>
2023-07-07 21:17:24 +0530

    authentication completed

--- vege/views.py
+++ vege/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from .models import Recipe
+from django.contrib.auth.models import User 
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='/login/')
 def recipes(request):
     # CHECKING IF REQUEST COMING IS POST OR GET
     if request.method=='POST':
@@ -53,3 +58,63 @@ def update(request, id):
 
     
     return render(request, 'vege/update.html', context={"recipe":recipe})
+
+
+
+def register(request):
+
+    if request.method == "POST":
+        fName = request.POST.get('firstName')
+        lName = request.POST.get('lastName')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        print(User.objects.filter(username=email))
+        user = User.objects.filter(username=email)
+        if user.exists():
+            messages.info(request, "User Already Exists")
+            return redirect ("/register/")
+            
+        user = User.objects.create_user(
+            username=email,
+            password=password,   
+        )
+
+        user.first_name=fName
+        user.last_name = lName 
+        user.save()
+
+        print(User.objects.all())
+        messages.info(request, "User Created Successfully")
+        return redirect('/register/')
+
+    return render(request, 'vege/register.html')
+
+
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        
+        # CHECK IF USERNAME EXIST IN DATABASE OR NOT 
+        if not User.objects.filter(username=username).exists():
+            messages.info(request, "Invalid Credentials")
+            return redirect('/login/')
+        
+        # CHECK PASSWORDS
+        user = authenticate(username=username, password=password)
+        if user is None:
+            messages.info(request, "Invalid Credentials")
+            return redirect('/login/')
+        
+        else:
+            # CREATING SESSION  
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'vege/login.html')
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('/login/')
